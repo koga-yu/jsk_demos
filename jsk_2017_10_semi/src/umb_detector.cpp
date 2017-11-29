0c286a92cb01a2e29bb89cda4baeaea847509958
Koga Yuya <yusquash67@ezweb.ne.jp>
2017-11-29 16:13:56 +0900

    trying to introduce template matching

--- jsk_2017_10_semi/src/umb_detector.cpp
+++ jsk_2017_10_semi/src/umb_detector.cpp
@@ -17,6 +17,25 @@ private:
         ROS_INFO("Received image");
         cv::Mat in_img = cv_bridge::toCvCopy(msg, msg->encoding)->image;
 
+        cv::Mat grayImage;
+        cv::cvtColor(in_img, grayImage, CV_BGR2GRAY);
+
+        cv::Mat result_img;// = new cv::Mat(in_img.Height - tmp_img.Height + 1, in_img.Width - tmp_img.Width + 1, MatrixType.F32C1);
+
+        cv::matchTemplate(in_img, tmp_img, result_img, CV_TM_SQDIFF);
+
+        cv::Rect roi_rect(0, 0, tmp_img.cols, tmp_img.rows);
+        cv::Point max_pt;
+        double maxVal;
+        cv::minMaxLoc(result_img, NULL, &maxVal, NULL, &max_pt);
+        roi_rect.x = max_pt.x;
+        roi_rect.y = max_pt.y;
+        cv::rectangle(in_img, roi_rect, cv::Scalar(0, 0, 255, 3));
+
+        cv::imshow("in_img", in_img);
+        cv::waitKey(1);
+
+        /*
         cv::Mat grayImage, binImage;
         cv::cvtColor(in_img, grayImage, CV_BGR2GRAY);
         cv::threshold(grayImage, binImage, 128.0, 255.0, CV_THRESH_OTSU);
@@ -44,6 +63,7 @@ private:
         }
         cv::imshow("in_img", in_img);
         cv::waitKey(1);
+        */
     }
 
 public:
@@ -54,6 +74,12 @@ public:
         cv::namedWindow("Fast", 1);
 
         chatter_pub = node_handle.advertise<jsk_2017_10_semi::umb_pos>("umb_pos", 1000);
+
+        tmp_img = cv::imread("/home/kogatti/semi_ws/src/jsk_demos/jsk_2017_10_semi/picture/umb_handle.png", CV_LOAD_IMAGE_GRAYSCALE);
+        if (tmp_img.empty()){
+            std::cout << "couldn't read the image. ./../picture/umb_handle.png" << std::endl;
+            return;
+        }
     }
 
     int calcDist(const int col[3], int r, int g, int b)
@@ -91,6 +117,8 @@ private:
     image_transport::ImageTransport it_;
     ros::NodeHandle node_handle;
     ros::Publisher chatter_pub;
+
+    cv::Mat tmp_img;
 
     double umb_length = 800.0;
     double ideal_height = 20.0;
